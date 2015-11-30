6aa6c03d71e7d89d69ffcf41d53a6c438bdcc929
MGVera <madelinegvera@gmail.com>
2015-11-30 22:13:00 +0000

    Done.

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -7,8 +7,8 @@ end
 
 
 
-rescue_princess_peach
-  puts status
+# rescue_princess_peach (I had to toggle this to comment, so that it would past my test. Before it gave me an incomplete lab because of the test not passing)
+#   puts status
 
 
 
@@ -19,5 +19,5 @@ rescue_princess_peach
 
 
 
-rescue_princess_peach
-  puts status
+
+# variables defined in method cannot be accessed outside of it.
